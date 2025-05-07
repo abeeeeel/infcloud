f4bae88e1f134904dfb7a139ef37d752b27841be
Abel <aoriachenjuanes@iesmordefuentes.com>
2025-05-07 09:46:12 +0200

    config.js update

--- config.js
+++ config.js
@@ -332,7 +332,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 // port, ... (client installed into Davical "htdocs" subdirectory;
 // works "out of the box", no additional setup required):
 var globalNetworkCheckSettings={
-	href: http://localhost:5232/,
+	href: "http://localhost:5232/",
 	timeOut: 90000,
 	lockTimeOut: 10000,
 	checkContentType: true,
